4932e08849ac233c4a863d34336f5be6d63e6f6b
David Domínguez López <daviddominguezlopez@gmail.com>
2020-11-17 12:07:29 +0100

    Inicio del proyecto

---
+++ src/main/java/com/formacionbdi/microservicios/app/examenes/controllers/ExamenController.java
@@ -0,0 +1,55 @@
+package com.formacionbdi.microservicios.app.examenes.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formacionbdi.microservicios.app.examenes.services.ExamenService;
+import com.formacionbdi.microservicios.commons.controllers.CommonController;
+import com.formacionbdi.microservicios.commons.examenes.models.entity.Examen;
+
+@RestController
+public class ExamenController extends CommonController<Examen, ExamenService> {
+
+	@PutMapping("/{id}")
+	public ResponseEntity<?> modificarExamen(@Valid @RequestBody Examen examen, BindingResult result,
+			@PathVariable Long id) {
+		if (result.hasErrors()) {
+			return validar(result);
+		}
+		Optional<Examen> o = commonService.findById(id);
+		if (!o.isPresent()) {
+			return ResponseEntity.noContent().build();
+		}
+		Examen examenDb = o.get();
+		examenDb.setNombre(examen.getNombre());
+
+		examenDb.getPreguntas().stream().filter(pregunta -> !examen.getPreguntas().contains(pregunta))
+				.forEach(examenDb::removePregunta);
+
+		examenDb.setPreguntas(examen.getPreguntas());
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(commonService.save(examenDb));
+	}
+
+	@GetMapping("/filtrar/{term}")
+	public ResponseEntity<?> buscarPorNombre(@PathVariable String term) {
+		List<Examen> examenes = this.commonService.findByNombre(term);
+		return ResponseEntity.ok(examenes);
+	}
+
+	@GetMapping("/asignaturas")
+	public ResponseEntity<?> listarAsignaturas() {
+		return ResponseEntity.ok(commonService.findAllAsignaturas());
+	}
+}
